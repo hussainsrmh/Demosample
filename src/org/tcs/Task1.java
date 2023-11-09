62e4374cccc93ff2e63052226cfbcd5ecc1866dc
hussainsrmh <hussainsrmh@gmail.com>
2023-11-09 10:32:52 +0530

    code

---
+++ src/org/tcs/Task1.java
@@ -0,0 +1,8 @@
+package org.tcs;
+
+public class Task1 {
+	public static void main(String[] args) {
+		System.out.println("Task1");
+	}
+
+}
---
+++ src/org/tcs/Task2.java
@@ -0,0 +1,8 @@
+package org.tcs;
+
+public class Task2 {
+	public static void main(String[] args) {
+		System.out.println("Task2");
+	}
+
+}
